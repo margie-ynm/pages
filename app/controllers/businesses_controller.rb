af3661877f33764c26e724e5ccb33f2fba909131
Jim Padilla and Margie Nevarez <jim@>
2017-02-04 15:39:59 -0800

    add edit and delete functionality for category

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,12 +1,4 @@
 class BusinessesController < ApplicationController
-  def index
-    @businesses = Business.all
-    render :index
-  end
 
-  def show
-    @business = Business.find(params[:id])
-    render :show
-  end
 
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,6 +23,26 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
   private
   def category_params
     params.require(:category).permit(:title)
